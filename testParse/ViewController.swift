ee1551c83e32ff216c4f479659da5ca43d09a391
anna.yowlu <anna.morlans@yowlu.com>
2015-12-30 17:13:39 +0100

    Initial Commit

---
+++ testParse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testParse
+//
+//  Created by Anna on 30/12/15.
+//  Copyright © 2015 Anna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
