b04bc545e1af1b4040045597f1af4b4c119af47e
tastyapp <antoine.beneteau@efrei.net>
2016-08-30 17:35:32 +0200

    yffgfg

--- CityGuide/IVViewController.swift
+++ CityGuide/IVViewController.swift
@@ -34,7 +34,7 @@ class IVViewController: UIViewController , UITableViewDelegate, UITableViewDataS
     var booooooooooooo = 1
     var hetal = 4
     
-    
+    //antoine
     let mySwitch = SevenSwitch()
     var powered: Bool = false
     var selectedPokemon = Pokemon(id: "", name: "")
